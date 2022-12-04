b6987910c205bd8a1facad0fc9ba00bdef1e2b05
Eniotnaa <antoinelelong2001@gmail.com>
2022-12-04 16:40:50 +0100

    creation des 5 pages de presentation (sans faire la barre d'etape)

--- app/src/main/java/com/example/botics/Presentation3.java
+++ app/src/main/java/com/example/botics/Presentation3.java
@@ -7,18 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Presentation1 extends AppCompatActivity {
+public class Presentation3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_presentation1);
+        setContentView(R.layout.activity_presentation3);
 
         Button suivant = (Button) findViewById(R.id.suivant);
         suivant.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Presentation1.this, Presentation2.class);
+                Intent intent = new Intent(Presentation3.this, Presentation4.class);
                 startActivity(intent);
             }
         });
